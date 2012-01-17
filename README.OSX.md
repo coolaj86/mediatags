0e318871918185a313229aaa507d8cb9e1beaf31
AJ ONeal <coolaj86@gmail.com>
2012-01-17 01:49:04 -0700

    more OS X support

--- README.OSX.md
+++ README.OSX.md
@@ -1,18 +1,63 @@
+Prereqs
+===
+
+    brew install hg
+    brew install poppler
+
 Fixes
 ===
 
+m4atags / AtomicParsley
+---
+
+There's currently no work-around to build the real `m4atags` from 
+source so there's a nodejs shim around AtomicParsely.
+
+  * md5sums aren't the same
+  * art output very nearly works (has a few trailing bytes that seem to work)
+
 Comment out AtomicParsley stuff at the bottom in `build-deps.sh`
+
+    brew install hg
+    hg clone https://bitbucket.org/wez/atomicparsley
+    cd atomicparsley/
+    autoconf & automake
+    ./configure  --disable-universal
+    make
+    make install
+    AtomicParsley 
+
+pdftags / poppler
+---
+
+Use `build-deps.sh` to download the source for libpoppler (and comment it out once it fails),
+but actually install it using `homebrew` and  to compile poppler rather than using 
+
+    brew install poppler
+    LDFLAGS="-L/usr/local/Cellar/fontconfig/2.8.0/lib ${LDFLAGS}"
+    CPPFLAGS="-I/usr/local/Cellar/fontconfig/2.8.0/include ${LDFLAGS}"
 
 _Bool has not been declared
 ---
 
     /usr/local/include/mutils/mutils.h:250: error: '_Bool' has not been declared
 
-mutils.h and mhash.h:
+/usr/local/include/mutils/mutils.h:
 
+    #if !defined(__MUTILS_H)
+    #define __MUTILS_H
+
+    // Added to satisfy OS X
     #if !defined(_Bool)
     #define _Bool bool
     #endif
+    // End custom addition
+
+    #include <mutils/mincludes.h>
+
+    #if defined(const)
+    #define __const const
+    #endif 
 
 'basename' has not been declared
 ---
---
+++ osx-lion/m4atags.js
@@ -0,0 +1,157 @@
+#!/usr/bin/env node
+// AtomicParsley "/Users/coolaj86/TODO2/coolaj86/Music/iTunes/iTunes Media/Music/Owl City/All Things Bright and Beautiful/12 How I Became the Sea.m4a" --textdata > ap-output.tags.txt
+// AtomicParsley "/Users/coolaj86/TODO2/coolaj86/Music/iTunes/iTunes Media/Music/Owl City/All Things Bright and Beautiful/12 How I Became the Sea.m4a" --test > ap-output.atoms.txt
+// AtomicParsley "/Users/coolaj86/TODO2/coolaj86/Music/iTunes/iTunes Media/Music/Owl City/All Things Bright and Beautiful/12 How I Became the Sea.m4a" --test | grep mdat
+// /Atom mdat @ (\d+) of size: (\d+), ends @ (\d+)/
+
+// TODO how are md5sums calculated in the C code? why are they different here?
+(function () {
+  "use strict";
+
+  var fs = require('fs')
+    , exec = require('child_process').exec
+    , crypto = require('crypto')
+    , fullpath = process.argv[2]
+    , mdatRegExp = /Atom mdat @ (\d+) of size: (\d+), ends @ (\d+)/
+    , covrRegExp = /Atom covr @ \d+ of size: \d+, ends @ \d+\n\s*(Atom data @ \d+ of size: \d+, ends @ \d+\n*\s*)+/
+    , covrDataRegExp = /Atom data @ (\d+) of size: (\d+), ends @ (\d+)/
+    , fd
+    ;
+
+  function getArt(err, stdout, stderr) {
+    var match
+      , lines
+      , art = []
+      ;
+
+    if (err) {
+      console.log(JSON.stringify(err));
+      return;
+    }
+
+    match = stdout.match(covrRegExp);
+
+    if (!match) {
+      console.log('No Cover Art');
+    }
+
+    lines = match[0].split('\n');
+    // lose the covr atom
+    lines.shift();
+    lines.forEach(function (line) {
+      match = line.match(covrDataRegExp);
+      if (!match) {
+        return;
+      }
+
+      art.push({
+          start: parseInt(match[1], 10) + 16
+          // TODO figure out how far off the end is
+        , end: parseInt(match[3], 10)
+      });
+
+      console.log('cover art:', match[1], match[3]);
+    });
+
+    art.forEach(function (a) {
+      fs.read(fd, new Buffer(a.end - a.start), 0, a.end - a.start, a.start, function (err, len, buf) {
+        fs.writeFileSync('something.jpg', buf, 'binary');
+      });
+    });
+  }
+
+  function getMd5sum(err, stdout, stderr) {
+    var match = stdout.match(mdatRegExp)
+      , start
+      , size
+      , end
+      , hash = crypto.createHash('md5')
+      , rstream
+      ;
+
+    if (err) {
+      console.log(JSON.stringify(err));
+      return;
+    }
+    getArt(err, stdout, stderr);
+
+    if (!match) {
+      console.log(JSON.stringify({ error: { message: "Could not find mdat atom" } }));
+      return;
+    }
+
+    // parseInt - nodejs issue #2554
+    // +8 - AtomicParsley simply counts wrong
+    start = parseInt(match[1], 10) + 16;
+    size = parseInt(match[2], 10);
+    end = start + size - 16; //parseInt(match[3], 10);
+
+    rstream = fs.createReadStream(fullpath, { encoding: 'binary', start: start, end: end });
+    rstream.on('error', function (err) {
+      console.log(JSON.stringify(err));
+    });
+    rstream.on('data', function (chunk) {
+      //console.log('chunk:', chunk.slice(0, 16));
+      hash.update(chunk);
+    });
+    rstream.on('end', function () {
+      console.log(hash.digest('hex'));
+    });
+  }
+
+  function parseToJson(err, txt) {
+    var lines
+      , line
+      , match
+      , i
+      , result
+      , key
+      , key2
+      , val
+      ;
+
+    if (err) {
+      console.log(JSON.stringify(err));
+      return;
+    }
+
+    result = {};
+    lines = txt.split('\n');
+
+    for (i = 0; i < lines.length; i += 1) {
+      line = lines[i];
+      match = line.match(/Atom\s*"(.*?)"\s*(\[.*?\]){0,1}\s*contains:\s*(.*)/);
+      if (!match) {
+        result[key] += '\n' + line;
+        continue;
+      }
+      //console.log(match);
+      key = match[1];
+      key2 = match[2];
+      val = match[3];
+      if (key2) {
+        //key = key + ' ' + key2;
+        key = key + key2.replace('com.apple.iTunes;', '');
+      }
+      result[key] = val;
+    }
+
+    if (result.covr) {
+      result.covr = true;
+    }
+
+    console.log(JSON.stringify(result, null, '  '));
+  }
+
+  fs.open(fullpath, 'r', function (err, _fd) {
+    if (err) {
+      console.log(JSON.stringify(err));
+      return;
+    }
+
+    fd = _fd;
+  });
+  exec('AtomicParsley \'' + fullpath + '\' --textdata', parseToJson);
+  //exec('AtomicParsley \'' + fullpath + '\' --test', getMd5sum);
+  //fs.readFile(fullpath, 'utf-8', parseToJson);
+}());
